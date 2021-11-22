fc7df4e03ba6e78f1572fa8cfbb66b0e3eaf63fa
jhon-marquez-cardenas <jhon.marquez.cardenas@alumnos.upm.es>
2021-11-22 18:11:51 -0500

    Delete MongoDB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentModule } from './student/student.module';
-import {MongooseModule} from '@nestjs/mongoose';
 import { ProductModule } from './product/product.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
